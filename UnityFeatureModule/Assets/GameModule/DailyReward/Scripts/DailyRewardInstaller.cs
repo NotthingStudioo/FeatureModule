f0a7d80a952cf9654ad2bb701e4aa8b8897d0d58
hoanghai145 <hoangthanhhai1308@gmail.com>
2024-08-09 12:32:55 +0700

    update dailyreward

---
+++ UnityFeatureModule/Assets/GameModule/DailyReward/Scripts/DailyRewardInstaller.cs
@@ -0,0 +1,9 @@
+namespace DailyReward.Scripts
+{
+    using Zenject;
+
+    public class DailyRewardInstaller : Installer<DailyRewardInstaller>
+    {
+        public override void InstallBindings() { }
+    }
+}

33ea241c784649cc1629f217065fc7d8d3583957
huylqnothing <huylqnothing@gmail.com>
2024-10-15 17:12:24 +0700

    Update Daily Reward

--- UnityFeatureModule/Assets/GameModule/DailyReward/Scripts/DailyRewardInstaller.cs
+++ UnityFeatureModule/Assets/GameModule/DailyReward/Scripts/DailyRewardInstaller.cs
@@ -1,14 +1,53 @@
 namespace DailyReward.GameModule.DailyReward.Scripts
 {
+    using System.Drawing.Text;
+    using Cysharp.Threading.Tasks;
+    using FeatureTemplate.Scripts.Services;
+    using GameFoundation.Scripts.UIModule.ScreenFlow.BaseScreen.Presenter;
+    using GameFoundation.Scripts.UIModule.ScreenFlow.Managers;
+    using GameFoundation.Scripts.UIModule.ScreenFlow.Signals;
+    using global::DailyReward.GameModule.DailyReward.Blueprints;
+    using global::DailyReward.GameModule.DailyReward.MVP;
     using global::DailyReward.GameModule.DailyReward.Signals;
     using Zenject;
 
-    public class DailyRewardInstaller : Installer<DailyRewardInstaller>
+    // Input T as popup screen
+    public class DailyRewardInstaller<T> : Installer<DailyRewardInstaller<T>> where T : IScreenPresenter
     {
+        private SignalBus     signalBus;
+        private ScreenManager screenManager;
+        private DailyRewardMiscParamBlueprint dailyRewardMiscParamBlueprint;
+        private FeatureDataState featureDataState;
+        private DailyRewardService dailyRewardService;
         public override void InstallBindings()
         {
             this.SignalDeclaration();
             this.Container.BindInterfacesAndSelfTo<DailyRewardService>().AsCached().NonLazy();
+            this.signalBus = this.Container.Resolve<SignalBus>();
+            this.screenManager = this.Container.Resolve<ScreenManager>();
+            this.dailyRewardMiscParamBlueprint = this.Container.Resolve<DailyRewardMiscParamBlueprint>();
+            this.featureDataState = this.Container.Resolve<FeatureDataState>();
+            this.dailyRewardService = this.Container.Resolve<DailyRewardService>();
+            this.AutoOpenScreen();
+        }
+
+        private void AutoOpenScreen()
+        {
+            this.signalBus.Subscribe<ScreenShowSignal>(this.OnScreenShowSignal);
+        }
+
+        protected async   void OnScreenShowSignal(ScreenShowSignal screenShowSignal)
+        {
+            await UniTask.WaitUntil(() => this.featureDataState.IsBlueprintAndLocalDataLoaded);
+            if (string.IsNullOrEmpty(this.dailyRewardMiscParamBlueprint.StartOnScreen)||!this.featureDataState.IsBlueprintAndLocalDataLoaded)
+            {
+                return;
+            }
+
+            if (screenShowSignal.ScreenPresenter.GetType().Name != this.dailyRewardMiscParamBlueprint.StartOnScreen) return;
+
+            if (!this.dailyRewardService.IsNewDay()) return;
+            this.screenManager.OpenScreen<DailyRewardPresenter>().Forget();
         }
 
         private void SignalDeclaration()

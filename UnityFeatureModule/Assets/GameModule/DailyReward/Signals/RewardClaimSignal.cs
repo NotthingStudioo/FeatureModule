699dc69c05c24161f8eb98a2a81eb6296358c9e1
huylqnothing <huylqnothing@gmail.com>
2024-10-21 14:32:48 +0700

    Update signal cast reward to interface

--- UnityFeatureModule/Assets/GameModule/DailyReward/Signals/RewardClaimSignal.cs
+++ UnityFeatureModule/Assets/GameModule/DailyReward/Signals/RewardClaimSignal.cs
@@ -1,11 +1,12 @@
 ﻿namespace GameModule.DailyReward.Signals
 {
     using System.Collections.Generic;
+    using FeatureTemplate.Scripts.RewardHandle;
     using GameModule.DailyReward.Blueprints;
 
     public class RewardClaimSignal
     {
-        public List<Reward> Reward;
+        public List<IRewardRecord> Reward;
         public int          Day;
     }
 }

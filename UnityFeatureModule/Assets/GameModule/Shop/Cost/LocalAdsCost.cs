2ebf18aedd513359b9dc7a693bbf35e29d173632
huylqnothing <huylqnothing@gmail.com>
2024-10-24 15:32:22 +0700

    Modify a cost

--- UnityFeatureModule/Assets/GameModule/Shop/Cost/LocalAdsCost.cs
+++ UnityFeatureModule/Assets/GameModule/Shop/Cost/LocalAdsCost.cs
@@ -3,14 +3,15 @@
     using FeatureTemplate.Scripts.Models.Controllers;
     using Zenject;
 
-    public class AdsCost : BaseCost
+    public class LocalAdsCost : BaseCost
     {
-        public override string                             Id => "Ads";
+        public override string                             Id => "Local_Ads_Count";
         [Inject] private FeatureTransactionDataController          featureTransactionDataController;
         [Inject] private FeatureInventoryDataControllerData featureInventoryDataControllerData;
 
         public override bool CanAfford(ICostRecord record) { return this.featureTransactionDataController.CheckTransaction(record) >= int.Parse(record.CostValue); }
 
+        // DO some think like try purchase, if false
         public override bool Purchase(ICostRecord record)
         {
             if (this.CanAfford(record))
